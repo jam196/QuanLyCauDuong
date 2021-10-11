e636a2c86ebe6680fc5d45bc940ac153fe002eb3
PC <profile.nvt@gmail.com>
2021-10-12 00:19:58 +0700

    Add project files.

---
+++ ContosoApp/App.xaml.cs
@@ -0,0 +1,122 @@
+//  ---------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+//  The MIT License (MIT)
+// 
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+// 
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+// 
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  ---------------------------------------------------------------------------------
+
+using QuanLyCauDuong.Views;
+using QuanLyCauDuong.ViewModels;
+using Repository;
+using Repository.Rest;
+using Repository.Sql;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Globalization;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace QuanLyCauDuong
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Gets the app-wide MainViewModel singleton instance.
+        /// </summary>
+        public static MainViewModel ViewModel { get; } = new MainViewModel();
+
+        /// <summary>
+        /// Pipeline for interacting with backend service or database.
+        /// </summary>
+        public static IContosoRepository Repository { get; private set; }
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App() => InitializeComponent();
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.
+        /// </summary>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            // Load the database.
+            if (ApplicationData.Current.LocalSettings.Values.TryGetValue(
+                "data_source", out object dataSource))
+            {
+                switch (dataSource.ToString())
+                {
+                    case "Rest": UseRest(); break;
+                    default: UseSqlite(); break;
+                }
+            }
+            else
+            {
+                UseSqlite();
+            }
+
+            // Prepare the app shell and window content.
+            AppShell shell = Window.Current.Content as AppShell ?? new AppShell();
+            shell.Language = ApplicationLanguages.Languages[0];
+            Window.Current.Content = shell;
+
+            if (shell.AppFrame.Content == null)
+            {
+                // When the navigation stack isn't restored, navigate to the first page
+                // suppressing the initial entrance animation.
+                shell.AppFrame.Navigate(typeof(CustomerListPage), null,
+                    new SuppressNavigationTransitionInfo());
+            }
+
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Configures the app to use the Sqlite data source. If no existing Sqlite database exists, 
+        /// loads a demo database filled with fake data so the app has content.
+        /// </summary>
+        public static void UseSqlite()
+        {
+            string demoDatabasePath = Package.Current.InstalledLocation.Path + @"\Assets\Contoso.db";
+            string databasePath = ApplicationData.Current.LocalFolder.Path + @"\Contoso.db";
+            if (!File.Exists(databasePath))
+            {
+                File.Copy(demoDatabasePath, databasePath);
+            }
+            var dbOptions = new DbContextOptionsBuilder<ContosoContext>().UseSqlite(
+                "Data Source=" + databasePath);
+            Repository = new SqlContosoRepository(dbOptions);
+        }
+
+        /// <summary>
+        /// Configures the app to use the REST data source. For convenience, a read-only source is provided. 
+        /// You can also deploy your own copy of the REST service locally or to Azure. See the README for details.
+        /// </summary>
+        public static void UseRest() =>
+            Repository = new RestContosoRepository("https://customers-orders-api-prod.azurewebsites.net/api/");
+    }
+}
